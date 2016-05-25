190aea986d7547ba094fd28cd3d4e3a83240fc07
devinxu <xuwenping3211@gmail.com>
2016-05-25 21:37:55 +0800

    做了一些小修改

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -69,7 +69,7 @@ class ViewController: UIViewController {
 
                 }
                 else {
-                   aspectRatioConstraint = nil
+                    aspectRatioConstraint = nil
                 }
             }
         }
