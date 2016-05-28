21d14e0812c47fb9fc499accdadd0508915541a3
devinxu <xuwenping3211@gmail.com>
2016-05-28 15:38:19 +0800

    修改了cacini工程，增加代码让detail view在iphone和ipad显示效果一样

--- Cacini/Cacini/ViewController.swift
+++ Cacini/Cacini/ViewController.swift
@@ -10,15 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if let ivc = segue.destinationViewController as? ImageViewContorller {
+        var destination = segue.destinationViewController as UIViewController
+        if let nvc = destination as? UINavigationController {
+            destination = nvc.visibleViewController!
+        }
+        
+        if let ivc = destination as? ImageViewContorller {
             if let identifier = segue.identifier {
                 switch identifier {
                 case "Woman":
                     ivc.imageURL = DemoURL.Character.womanURL
+                    ivc.title = "woman"
                 case "Man":
                     ivc.imageURL = DemoURL.Character.manURL
+                    ivc.title = "man"
                 case "Child":
                     ivc.imageURL = DemoURL.Character.childURL
+                    ivc.title = "child"
                 default:
                     break
                 }
